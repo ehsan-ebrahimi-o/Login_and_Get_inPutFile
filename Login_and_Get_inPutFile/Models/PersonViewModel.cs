b318831cc71e5929628fd15bde2ffb940707443e
ehsan-ebrahimi <ehsanebrahimi1977@gmail.com>
2024-01-08 00:30:52 +0330

    Add project files.

---
+++ Login_and_Get_inPutFile/Models/PersonViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Login_and_Get_inPutFile.Models
+{
+    public class PersonViewModel
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Age { get; set; }
+
+        public string CityBurn { get; set; }
+
+        public string Aboutself { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Password { get; set; }
+
+        public IFormFile File { get; set; }
+
+        public string ImageName { get; set; }
+    }
+}
